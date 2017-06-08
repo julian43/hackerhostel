298b669a78949ebb551cdcdf9805e2190fb97758
Julian Cowell <jgarth123@icloud.com>
2017-06-08 16:44:22 -0500

    horrible hack

--- assgn1/fun.js
+++ assgn1/fun.js
@@ -1,10 +1,38 @@
-function show_image(src, width, height, alt) {
-  var img = document.createElement("img");
-  img.src = src;
-  img.width = width;
-  img.height = height;
-  img.alt = alt;
+function show_image_thrice(src, width, height, alt) {
+  imgdiv = document.getElementById('imgdiv')
+  imgdiv.innerHTML = "";
+  function get_img_tag(num){
+    var img = document.createElement("img");
+    img.src = src+num;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
 
-  // This next line will just add it to the <body> tag
-  document.body.appendChild(img);
+    return img;
+  }
+
+  //Horrible Hack!!
+  imgdiv.appendChild(get_img_tag("1.jpg"));
+  imgdiv.appendChild(get_img_tag("2.jpg"));
+  imgdiv.appendChild(get_img_tag("3.jpg"));
+}
+
+
+//Better
+
+function show_cat_images(partial_src, width, height, alt, which){
+  function get_img_tag(src){
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+
+    return img;
+  }
+
+  for(var i=0;i<which.length;i++){
+    console.log(partial_src+which[i])
+    document.body.appendChild(get_img_tag(partial_src+which[i]))
+  }
 }
